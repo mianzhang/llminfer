06657f88e56c240270eeda3f1484897b2e960125
Mian Zhang <mxz220020@csr-096799.utdallas.edu>
2025-09-11 10:51:18 -0500

    fix package setup

--- example.py
+++ example.py
@@ -1,11 +1,15 @@
 import llminfer
 
+
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
 llminfer.process_jsonl(
     "prompts.jsonl",
     "output.jsonl", 
-    provider="openai",
-    model="gpt-4",
+    provider="vllm",
+    model="/localdisk/models/Qwen2.5-1.5B-Instruct",
     input_key="prompt",  # Key pointing to string prompts
     temperature=0.7,
-    max_workers=10
 )
